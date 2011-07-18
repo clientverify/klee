ab5408f748ee9047ca21fde77d497d2edd15b6a8
rac <rac@cs.unc.edu>
2011-07-18 11:11:35 -0400

    MemoryObjectNodes now have references to their incoming edges

--- lib/Cliver/TestHelper.cpp
+++ lib/Cliver/TestHelper.cpp
@@ -32,39 +32,41 @@ void ExternalHandler_test_extract_pointers(klee::Executor* executor,
 		klee::ExecutionState *state, klee::KInstruction *target, 
     std::vector<klee::ref<klee::Expr> > &arguments) {
 
+	AddressSpaceGraph *asg_build = new AddressSpaceGraph(&state->addressSpace);
+	asg_build->build_graph();
+	delete asg_build;
+
 	AddressSpaceGraph *asg = new AddressSpaceGraph(&state->addressSpace);
 
 	for (klee::MemoryMap::iterator it=state->addressSpace.objects.begin(),
 			ie=state->addressSpace.objects.end(); it!=ie; ++it) {
 		MemoryObjectNode *a = new MemoryObjectNode(it->second);
 		MemoryObjectNode *b = new MemoryObjectNode(it->second);
-		asg->extract_pointers(*it->second, a);
-		asg->extract_pointers_by_resolving(*it->second, b);
-		if (a->degree != b->degree) {
+		asg->extract_pointers(a);
+		asg->extract_pointers_by_resolving(b);
+		if (a->out_degree() != b->out_degree()) {
 			cv_warning("AddressSpaceGraph Test: degree mismatch %d != %d",
-					a->degree, b->degree);
+					a->out_degree(), b->out_degree());
 			(*it->second).print(*cv_warning_stream);
 			return;
 		} else {
-			PointerEdge *a_edge=a->first_edge, *b_edge=b->first_edge;
-			while (a_edge != NULL && b_edge != NULL) {
+			for (unsigned i=0; i<a->out_degree(); ++i) {
+				PointerEdge *a_edge=a->out_edge(i), *b_edge=b->out_edge(i);
 				if (a_edge->offset != b_edge->offset) {
 					cv_warning("AddressSpaceGraph Test: edge offset mismatch %d != %d",
 						a_edge->offset, b_edge->offset);
 					(*it->second).print(*cv_warning_stream);
 					return;
-				} 
-				else if (a_edge->points_to_address != b_edge->points_to_address) {
+				} else if (a_edge->points_to_address != b_edge->points_to_address) {
 					cv_warning("AddressSpaceGraph Test: edge points_to_address mismatch %ld != %ld",
 						a_edge->points_to_address, b_edge->points_to_address);
 					(*it->second).print(*cv_warning_stream);
 					return;
 				}
-				a_edge = a_edge->next;
-				b_edge = b_edge->next;
 			}
 		} 
 	}
+	delete asg;
 	cv_message("test_extract_pointers: PASSED");
 }
 
