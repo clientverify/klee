f1abc81c87bfda0c5288213ab06c0a113d1e6b0a
rac <rac@cs.unc.edu>
2011-08-07 19:50:01 -0400

    Added TrainingSearcher and support

--- lib/Cliver/CVSearcher.h
+++ lib/Cliver/CVSearcher.h
@@ -32,12 +32,26 @@ class CVSearcher : public klee::Searcher {
 	void printName(std::ostream &os) {
 		os << "CVSearcher\n";
 	}
- private:
+ protected:
 	int state_count();
 	ExecutionStatePropertyMap states_;
 	ExecutionStateProperty* current_property_;
 	klee::Searcher* base_searcher_;
 	StateMerger* merger_;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TrainingSearcher : public CVSearcher {
+ public:
+	TrainingSearcher(klee::Searcher* base_searcher, StateMerger* merger);
+
+	klee::ExecutionState &selectState();
+	void printName(std::ostream &os) {
+		os << "TrainingSearcher\n";
+	}
+ private:
+	TrainingPhaseProperty* phases_[MAX_TRAINING_PHASE];
 };
 
 ////////////////////////////////////////////////////////////////////////////////
