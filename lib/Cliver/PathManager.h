452eb7d5153d3e0e4c4e34b5298b43a107f6557b
Robby Cochran <rac@cs.unc.edu>
2011-08-12 16:03:13 -0400

    Added PathRange object to PathManager

--- lib/Cliver/PathManager.h
+++ lib/Cliver/PathManager.h
@@ -9,12 +9,14 @@
 #ifndef PATH_MANAGER_H
 #define PATH_MANAGER_H
 
-#include "CVExecutionState.h"
 #include "klee/Internal/Module/InstructionInfoTable.h"
 #include "klee/Internal/Module/KInstruction.h"
+#include "ClientVerifier.h"
 
 #include <list>
+#include <set>
 #include <fstream>
+#include <iostream>
 
 #include <boost/serialization/access.hpp>
 #include <boost/serialization/vector.hpp>
@@ -32,15 +34,18 @@ struct FunctionCallBranchEvent {
 
 class Path {
  public:
+	friend class PathManager;
 	typedef std::vector<bool> path_ty;
 	typedef path_ty::const_iterator path_iterator;
 
 	Path();
 	~Path();
+	unsigned size() const;
 	path_iterator begin() const;
 	path_iterator end() const;
 	void add(bool direction, klee::KInstruction* inst);
 	bool less(const Path &b) const;
+	bool equal(const Path &b) const;
 	void inc_ref();
 	void dec_ref();
 	unsigned ref() { return ref_count_; }
@@ -62,27 +67,83 @@ class Path {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+class PathRange {
+ public:
+	typedef std::pair<klee::KInstruction*, klee::KInstruction*> kinst_pair_ty;
+	typedef std::pair<llvm::Instruction*, llvm::Instruction*> inst_pair_ty;
+	typedef std::pair<unsigned, unsigned> inst_id_pair_ty;
+
+	PathRange(klee::KInstruction* s, klee::KInstruction* e);
+	PathRange();
+	bool less(const PathRange &b) const;
+	bool equal(const PathRange &b) const;
+	int compare(const PathRange &b) const;
+
+	klee::KInstruction* start() { return start_; }
+	klee::KInstruction* end() { return end_; }
+
+	inst_id_pair_ty ids() const;
+	inst_pair_ty 		insts() const;
+	kinst_pair_ty 	kinsts() const;
+
+	void print(std::ostream &os) const;
+
+ private:
+	klee::KInstruction* start_;
+	klee::KInstruction* end_;
+};
+
+inline std::ostream &operator<<(std::ostream &os, 
+		const PathRange &pr) {
+  pr.print(os);
+  return os;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+class SocketEvent;
+
 class PathManager {
  public:
+	typedef std::set<const SocketEvent*> message_set_ty;
 	PathManager();
 	PathManager(const PathManager &pm);
 	PathManager* clone();
+	unsigned length() { return path_->size(); }
+	bool merge(const PathManager &pm);
 	bool less(const PathManager &b) const;
 	void add_false_branch(klee::KInstruction* inst);
 	void add_true_branch(klee::KInstruction* inst);
 	void add_branch(bool direction, klee::KInstruction* inst);
 	const Path& get_consolidated_path();
 	void print_diff(const PathManager &b, std::ostream &os) const;
+	void print(std::ostream &os) const;
+	bool add_message(const SocketEvent* se);
+	void set_range(const PathRange& range);
+	const message_set_ty& messages() { return messages_; }
 
  private:
 	Path* path_;
+	PathRange range_;
+	message_set_ty messages_;
 };
 
+inline std::ostream &operator<<(std::ostream &os, 
+		const PathManager &p) {
+  p.print(os);
+  return os;
+}
+ 
 class PathManagerFactory {
  public:
   static PathManager* create();
 };
 
+struct PathManagerLT {
+	bool operator()(const PathManager* a, const PathManager* b) const;
+};
+
+typedef std::set<PathManager*, PathManagerLT> PathSet;
 
 ////////////////////////////////////////////////////////////////////////////////
 
