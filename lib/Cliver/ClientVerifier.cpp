3c5cf23149e9f1691404d666e78fc417137b9aa1
rac <rac@cs.unc.edu>
2011-08-13 22:12:26 -0400

    Renamed TrainingSearcher to TrainingMergeSearcher

--- lib/Cliver/ClientVerifier.cpp
+++ lib/Cliver/ClientVerifier.cpp
@@ -196,8 +196,8 @@ void ClientVerifier::register_events(CVExecutor *executor) {
 			break;
 		case DefaultTrainingMode:
 		case TetrinetTrainingMode:
-			pre_event_callbacks_.connect(&TrainingSearcher::handle_pre_event);
-			post_event_callbacks_.connect(&TrainingSearcher::handle_post_event);
+			pre_event_callbacks_.connect(&TrainingMergeSearcher::handle_pre_event);
+			post_event_callbacks_.connect(&TrainingMergeSearcher::handle_post_event);
 			break;
 	}
 }
@@ -226,7 +226,7 @@ CVSearcher* ClientVerifier::construct_searcher() {
 		case DefaultTrainingMode:
 		case TetrinetTrainingMode:
 			merger_ = new SymbolicStateMerger(pruner_);
-			searcher_ = new TrainingSearcher(new klee::DFSSearcher(), merger_);
+			searcher_ = new TrainingMergeSearcher(new klee::DFSSearcher(), merger_);
 			break;
 	}
 	return searcher_;
