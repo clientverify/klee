7d1102807bd1e521f2b9576fb041cfa34bc8270f
rac <rac@cs.unc.edu>
2011-08-21 11:27:57 -0400

    Pulled ExecutionStateProperty out of CVExecutionState into a new file

--- lib/Cliver/CVExecutionState.cpp
+++ lib/Cliver/CVExecutionState.cpp
@@ -11,61 +11,10 @@
 #include "CVStream.h"
 #include "NetworkManager.h"
 #include "PathManager.h"
-#include "ClientVerifier.h"
 #include "../Core/Common.h"
 #include "llvm/Support/raw_ostream.h"
 
-#include <boost/foreach.hpp>
-#define foreach BOOST_FOREACH 
-
 namespace cliver {
-	
-////////////////////////////////////////////////////////////////////////////////
-
-bool ExecutionStatePropertyLT::operator()(const ExecutionStateProperty* a, 
-		const ExecutionStateProperty* b) const {
-	return a->compare(*b) < 0;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-LogIndexProperty::LogIndexProperty() : socket_log_index(-1) {}
-
-int LogIndexProperty::compare(const ExecutionStateProperty &b) const {
-	const LogIndexProperty *_b = static_cast<const LogIndexProperty*>(&b);
-	return socket_log_index - _b->socket_log_index;
-}
-
-void LogIndexProperty::print(std::ostream &os) const {
-	os << "log index = " << socket_log_index;
-}
-
-//////////////////////////////////////////////////////////////////////////////
-
-TrainingProperty::TrainingProperty() 
-	: training_round(0),
-	  training_state(TrainingProperty::PrepareExecute) {}
-
-int TrainingProperty::compare(const ExecutionStateProperty &b) const {
-	const TrainingProperty *_b = static_cast<const TrainingProperty*>(&b);
-
-	if (training_round != _b->training_round)
-		return training_round - _b->training_round;
-
-	if (training_state != _b->training_state)
-		return training_state - _b->training_state;
-
-	return path_range.compare(_b->path_range);
-}
-
-void TrainingProperty::print(std::ostream &os) const {
-	os << "[round: " << training_round
-	   << ", range: " << path_range
-	   //<< ", trainingstate: " << training_state
-		 << "]";
-}
-
-////////////////////////////////////////////////////////////////////////////////
 
 int CVExecutionState::next_id_ = 0;
 
@@ -139,21 +88,6 @@ void CVExecutionState::reset_path_manager() {
 bool CVExecutionStateLT::operator()(const CVExecutionState* a, 
 		const CVExecutionState* b) const {
 	return a->compare(*b) < 0;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-ExecutionStateProperty* ExecutionStatePropertyFactory::create() {
-	switch (g_cliver_mode) {
-		case DefaultMode:
-		case TetrinetMode: 
-			return new LogIndexProperty();
-			break;
-		case DefaultTrainingMode: 
-			return new TrainingProperty();
-	}
-	cv_error("invalid cliver mode");
-	return NULL;
 }
 
 } // End cliver namespace
---
+++ lib/Cliver/ExecutionStateProperty.h
@@ -0,0 +1,103 @@
+//===-- ExecutionStateProperty.h --------------------------------*- C++ -*-===//
+//
+// <insert license>
+//
+//===----------------------------------------------------------------------===//
+//
+//===----------------------------------------------------------------------===//
+#ifndef CLIVER_EXECUTION_STATE_PROPERTY_H
+#define CLIVER_EXECUTION_STATE_PROPERTY_H
+
+#include "PathManager.h"
+#include <map>
+#include <set>
+#include <iostream>
+
+namespace cliver {
+class CVExecutionState;
+
+////////////////////////////////////////////////////////////////////////////////
+
+/// Classes that inherit from ExecutionStateProperty act as external "decorators"
+/// for CVExecutionStates to describe a certain set of differentiating properties.
+/// Each CVExecutionState has an internal ExecutionStateProperty, but a single
+/// instance of this class can be used to represent a class of states when combined
+/// with stl collections. This is how ExecutionStateProperty is used in the 
+/// CVSearcher classes. Instances of ExecutionStateProperty are used to form
+/// an ordering of CVExecutionStates, for example, LogIndexProperty uses the 
+/// log_index of the NetworkManager to form a class of CVExecutionStates.
+/// ExecutionStateProperty should be used as if it was pure virtual, i.e., not
+/// used directly. The member variables of these classes are public so that 
+/// a CVExecutionState is not needed to create an instance. Inherited classes
+/// should preserve this functionality through public member variables or functions
+/// to create instances without a given CVExecutionState.
+///
+class ExecutionStateProperty {
+ public:
+  virtual void print(std::ostream &os) const {};
+	virtual int compare(const ExecutionStateProperty &p) const { assert(0); return 0; }
+	virtual ExecutionStateProperty* clone() { assert(0); return NULL; }
+};
+
+inline std::ostream &operator<<(std::ostream &os, 
+		const ExecutionStateProperty &p) {
+  p.print(os);
+  return os;
+}
+ 
+struct ExecutionStatePropertyLT {
+	bool operator()(const ExecutionStateProperty* a, 
+			const ExecutionStateProperty* b) const;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+typedef std::set<CVExecutionState*> ExecutionStateSet;
+
+typedef std::map<ExecutionStateProperty*,
+								 ExecutionStateSet,
+								 ExecutionStatePropertyLT> ExecutionStatePropertyMap;
+
+struct ExecutionStatePropertyFactory {
+	static ExecutionStateProperty* create();
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+class LogIndexProperty : public ExecutionStateProperty {
+ public: 
+	LogIndexProperty();
+	LogIndexProperty* clone() { return new LogIndexProperty(*this); }
+  void print(std::ostream &os) const;
+	int compare(const ExecutionStateProperty &b) const;
+
+	// Property values
+	int socket_log_index;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TrainingProperty : public ExecutionStateProperty {
+ public: 
+	enum TrainingState {
+		PrepareExecute=0, 
+		Execute, 
+		Merge, 
+		NetworkClone, 
+		EndState
+	};
+	TrainingProperty();
+	TrainingProperty* clone() { return new TrainingProperty(*this); }
+  void print(std::ostream &os) const;
+	int compare(const ExecutionStateProperty &b) const;
+
+	// Property values
+	int training_round;
+	TrainingState training_state;
+	PathRange path_range;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // end namespace cliver
+#endif // CLIVER_EXECUTION_STATE_PROPERTY_H
