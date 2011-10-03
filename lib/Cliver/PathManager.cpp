93c2d4cf64f226a8796faa63a29a1e3fba982874
Robby Cochran <Robby Cochran>
2011-10-03 15:47:44 -0400

    Added debug print statement in VerifyPathManager::try_branch

--- lib/Cliver/PathManager.cpp
+++ lib/Cliver/PathManager.cpp
@@ -207,7 +207,12 @@ bool VerifyPathManager::less(const PathManager &b) const {
 bool VerifyPathManager::try_branch(bool direction, 
 		klee::Solver::Validity validity, klee::KInstruction* inst) {
 	assert(path_ && "path is null");
+	assert(index_ < path_->length());
 	if (index_ < path_->length()) {
+    if (direction != path_->get_branch(index_)) {
+			CVDEBUG("Failed after covering " << (float)index_/path_->length()
+					<< " of branches (" << index_ <<"/"<< path_->length());
+		}
 		return direction == path_->get_branch(index_);
 	}
 	return false;
@@ -246,7 +251,8 @@ bool VerifyPrefixPathManager::merge(const PathManager &pm) {
 }
 
 bool VerifyPrefixPathManager::less(const PathManager &b) const {
-	const VerifyPrefixPathManager *vpm = static_cast<const VerifyPrefixPathManager*>(&b);
+	const VerifyPrefixPathManager *vpm 
+		= static_cast<const VerifyPrefixPathManager*>(&b);
 	if (range_.less(vpm->range_))
 		return true;
 	return path_->less(*(vpm->path_));
