0adcddf8477a0d481ec132d39a2b7410ece871e9
Robby Cochran <Robby Cochran>
2011-11-05 21:16:30 -0400

    CVExecutionState, CVExecutor: Removed references to PathTree, PathTree is now only handled by PathManager and CVSearcher

--- lib/Cliver/CVExecutionState.cpp
+++ lib/Cliver/CVExecutionState.cpp
@@ -13,7 +13,6 @@
 #include "ExecutionStateProperty.h"
 #include "NetworkManager.h"
 #include "PathManager.h"
-#include "PathTree.h"
 
 #include "../Core/Common.h"
 #include "llvm/Support/raw_ostream.h"
@@ -61,7 +60,6 @@ void CVExecutionState::initialize(CVExecutor *executor) {
 	network_manager_ = NetworkManagerFactory::create(this);
 	path_manager_ = PathManagerFactory::create();
 	property_ = ExecutionStatePropertyFactory::create();
-	path_tree_ = PathTreeFactory::create(this);
 }
 
 CVExecutionState* CVExecutionState::clone() {
@@ -71,7 +69,6 @@ CVExecutionState* CVExecutionState::clone() {
 		= network_manager_->clone(cloned_state); 
 	cloned_state->path_manager_ = path_manager_->clone();
   cloned_state->property_ = property_->clone();
-	cloned_state->path_tree_ = path_tree_;
 
   return cloned_state;
 }
@@ -79,7 +76,8 @@ CVExecutionState* CVExecutionState::clone() {
 CVExecutionState* CVExecutionState::branch() {
   depth++;
   CVExecutionState *branched_state = clone();
-	path_tree_->add_branched_state(this, branched_state);
+	// Alert path manager of state branch event
+	path_manager_->state_branch(this, branched_state);
   branched_state->coveredNew = false;
   branched_state->coveredLines.clear();
   weight *= .5;
@@ -95,13 +93,6 @@ void CVExecutionState::reset_path_manager(PathManager* path_manager) {
 		path_manager_ = path_manager;
 	else
 		path_manager_ = PathManagerFactory::create();
-}
-
-void CVExecutionState::reset_path_tree(PathTree* path_tree) {
-	if (path_tree)
-		path_tree_ = path_tree;
-	else
-		path_tree_ = PathTreeFactory::create(this);
 }
 
 bool CVExecutionStateLT::operator()(const CVExecutionState* a, 
