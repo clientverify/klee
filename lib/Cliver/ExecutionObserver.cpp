90236334c4376686586440585f41acfc28c5c261
Robby Cochran <Robby Cochran>
2012-02-02 11:59:05 -0500

    Added ExecutionObserver class

---
+++ lib/Cliver/ExecutionObserver.cpp
@@ -0,0 +1,45 @@
+//===-- ExecutionObserver.cpp -----------------------------------*- C++ -*-===//
+//
+// <insert license>
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//===----------------------------------------------------------------------===//
+
+#include "ExecutionObserver.h"
+#include "CVCommon.h"
+#include "CVStream.h"
+#include "CVExecutionState.h"
+
+namespace cliver {
+
+////////////////////////////////////////////////////////////////////////////////
+
+ExecutionEvent::ExecutionEvent(ExecutionEventType t, 
+                               CVExecutionState* s, 
+                               CVExecutionState* p) 
+  : event_type(t), state(s), parent(p) {}
+
+ExecutionEvent::ExecutionEvent(ExecutionEventType t, 
+                               klee::ExecutionState* s, 
+                               klee::ExecutionState* p)
+  : event_type(t), 
+    state(static_cast<CVExecutionState*>(s)), 
+    parent(static_cast<CVExecutionState*>(p)) {}
+
+///////////////////////////////////////////////////////////////////////////////
+
+void ExecutionObserverPrinter::notify(ExecutionEvent ev) {
+#define X(x) #x,
+	static std::string execution_event_types[] = { CV_EXECUTION_EVENT_TYPES };
+#undef X
+
+  switch(ev.event_type) {
+#define X(x) case x : { CVDEBUG( #x ); break; }
+    CV_EXECUTION_EVENT_TYPES
+#undef X
+  }
+}
+
+} // end namespace cliver
---
+++ lib/Cliver/ExecutionObserver.h
@@ -0,0 +1,84 @@
+//===-- ExecutionObserver.h -------------------------------------*- C++ -*-===//
+//
+// <insert license>
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//===----------------------------------------------------------------------===//
+#ifndef CLIVER_EXECUTION_OBSERVER_H
+#define CLIVER_EXECUTION_OBSERVER_H
+
+#include <list>
+
+namespace klee {
+class ExecutionState;
+}
+
+namespace cliver {
+
+class CVExecutionState;
+
+////////////////////////////////////////////////////////////////////////////////
+
+#define CV_EXECUTION_EVENT_TYPES \
+  X(CV_BRANCH)                   \
+  X(CV_BRANCH_UNCONDITIONAL)     \
+  X(CV_BRANCH_INTERNAL_TRUE)     \
+  X(CV_BRANCH_INTERNAL_FALSE)    \
+  X(CV_BRANCH_TRUE)              \
+  X(CV_BRANCH_FALSE)             \
+  X(CV_STATE_FORK)               \
+  X(CV_STATE_CLONE)              \
+  X(CV_STATE_REMOVED)            \
+  X(CV_RETURN)                   \
+  X(CV_CALL)                     \
+  X(CV_CALL_EXTERNAL)            \
+  X(CV_TRANSFER_TO_BASICBLOCK)   \
+  X(CV_TRANSFER_TO_BASICBLOCK_LOOP)   \
+  X(CV_BASICBLOCK_CHANGED)       \
+  X(CV_BASICBLOCK_ENTRY)         \
+  X(CV_SOCKET_CREATE)            \
+  X(CV_SOCKET_SHUTDOWN)          \
+  X(CV_SOCKET_WRITE)             \
+  X(CV_SOCKET_READ)
+
+#define X(x) x,
+enum ExecutionEventType {
+  CV_EXECUTION_EVENT_TYPES CV_NULL_EVENT
+};
+#undef X
+
+class ExecutionEvent {
+ public:
+  CVExecutionState* state;
+  CVExecutionState* parent;
+  ExecutionEventType event_type;
+
+  ExecutionEvent(ExecutionEventType t, 
+                 CVExecutionState* s, CVExecutionState* p = NULL);
+
+  ExecutionEvent(ExecutionEventType t, 
+                 klee::ExecutionState* s, klee::ExecutionState* p = NULL);
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+class ExecutionObserver {
+ public:
+  ExecutionObserver() {}
+  virtual ~ExecutionObserver() {}
+  virtual void notify(ExecutionEvent ev) = 0;
+};
+
+class ExecutionObserverPrinter : public ExecutionObserver {
+ public:
+  ExecutionObserverPrinter() {}
+  virtual ~ExecutionObserverPrinter() {}
+  virtual void notify(ExecutionEvent ev);
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // end namespace cliver
+#endif // CLIVER_EXECUTION_OBSERVER_H
