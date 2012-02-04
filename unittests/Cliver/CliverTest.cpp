e48dfdb4e2a71122bd32da4537e16dd7429b35f4
Robby Cochran <Robby Cochran>
2012-02-04 16:21:27 -0500

    Added basic tempate for Cliver unittests

---
+++ unittests/Cliver/CliverTest.cpp
@@ -0,0 +1,22 @@
+//===-- ExprTest.cpp ------------------------------------------------------===//
+//
+//                     The KLEE Symbolic Virtual Machine
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include <iostream>
+#include "gtest/gtest.h"
+
+#include "klee/Expr.h"
+
+using namespace klee;
+//using namespace cliver;
+
+namespace {
+
+TEST(CliverTest, ClientVerifierConstructionDestruction) {}
+
+}
