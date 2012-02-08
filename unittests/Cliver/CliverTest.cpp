4b3a163e8823fd85c28c0ecebb8e158ab8ca3d64
Robby Cochran <rac@cs.unc.edu>
2012-02-08 17:09:25 -0500

    EditDistance unittests

--- unittests/Cliver/CliverTest.cpp
+++ unittests/Cliver/CliverTest.cpp
@@ -43,6 +43,12 @@ struct EditDistanceTableData {
 };
 
 typedef EditDistanceTable<Score<string,int>,string,int> StringEDT;
+typedef EditDistanceRow<Score<string,int>,string,int> StringEDR;
+typedef EditDistanceUkkonen<Score<string,int>,string,int> StringEDU;
+typedef EditDistanceUKK<Score<string,int>,string,int> StringEDUKK;
+typedef EditDistanceDynamicUKK<Score<string,int>,string,int> StringEDDynamicUKK;
+typedef EditDistanceStaticUKK<Score<string,int>,string,int> StringEDStaticUKK;
+typedef EditDistanceFullUKK<Score<string,int>,string,int> StringEDFullUKK;
 typedef EditDistanceTableData<std::string, int> StringEDTData;
 
 std::ostream& operator<<(std::ostream &os, const std::vector<int> &v) {
@@ -104,47 +110,134 @@ TEST(CliverTest, ClientVerifierConstructionDestruction) {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+TEST(CliverTest, EditDistance0) {
+  std::string s = "ACCGGTCGGC";
+  std::string t = "TGGTCGCCC";
+  StringEDT edt(s, t);
+  StringEDR edr(s, t);
+  StringEDU edu(s, t);
+  StringEDUKK edukk(s, t);
+  StringEDDynamicUKK eddukk(s, t);
+  StringEDFullUKK edsukk(s, t);
+
+  int cost_t = edt.compute_editdistance();
+  int cost_r = edr.compute_editdistance();
+  int cost_u = edu.compute_editdistance();
+  int cost_ukk = edukk.compute_editdistance();
+  int cost_dukk = eddukk.compute_editdistance();
+  int cost_sukk = edsukk.compute_editdistance();
+
+  ASSERT_EQ(cost_t, cost_r);
+  ASSERT_EQ(cost_t, cost_u);
+  ASSERT_EQ(cost_t, cost_ukk);
+  ASSERT_EQ(cost_t, cost_dukk);
+  ASSERT_EQ(cost_t, cost_sukk);
+}
+
 TEST(CliverTest, EditDistance1) {
   StringEDT edt(test1.s, test1.t);
-  edt.compute_editdistance();
+  StringEDR edr(test1.s, test1.t);
+  StringEDU edu(test1.s, test1.t);
+  StringEDUKK edukk(test1.s, test1.t);
+  StringEDDynamicUKK eddukk(test1.s, test1.t);
+  StringEDFullUKK edsukk(test1.s, test1.t);
 
-  ASSERT_EQ(test1.costs, edt.costs());
+
+  int cost_t = edt.compute_editdistance();
+  int cost_r = edr.compute_editdistance();
+  int cost_u = edu.compute_editdistance();
+  int cost_ukk = edukk.compute_editdistance();
+  int cost_dukk = eddukk.compute_editdistance();
+  int cost_sukk = edsukk.compute_editdistance();
+
+  ASSERT_EQ(cost_t, cost_r);
+  ASSERT_EQ(cost_t, cost_u);
+  ASSERT_EQ(cost_t, cost_ukk);
+  ASSERT_EQ(cost_t, cost_dukk);
+  ASSERT_EQ(cost_t, cost_sukk);
 }
 
 TEST(CliverTest, EditDistance2) {
   StringEDT edt(test2.s, test2.t);
-  edt.compute_editdistance();
+  StringEDR edr(test2.s, test2.t);
+  StringEDU edu(test2.s, test2.t);
+  StringEDUKK edukk(test2.s, test2.t);
+  StringEDDynamicUKK eddukk(test2.s, test2.t);
+  StringEDFullUKK edsukk(test2.s, test2.t);
 
-  ASSERT_EQ(test2.costs, edt.costs());
+  int cost_t = edt.compute_editdistance();
+  int cost_r = edr.compute_editdistance();
+  int cost_u = edu.compute_editdistance();
+  int cost_ukk = edukk.compute_editdistance();
+  int cost_dukk = eddukk.compute_editdistance();
+  int cost_sukk = edsukk.compute_editdistance();
+
+  ASSERT_EQ(cost_t, cost_r);
+  ASSERT_EQ(cost_t, cost_u);
+  ASSERT_EQ(cost_t, cost_ukk);
+  ASSERT_EQ(cost_t, cost_dukk);
+  ASSERT_EQ(cost_t, cost_sukk);
 }
 
-TEST(CliverTest, EditDistance3) {
+TEST(CliverTest, EditDistanceUKK) {
   StringEDT edt(test3.s, test3.t);
-  edt.compute_editdistance();
+  StringEDR edr(test3.s, test3.t);
+  StringEDU edu(test3.s, test3.t);
+  StringEDUKK edukk(test3.s, test3.t);
+  StringEDDynamicUKK eddukk(test3.s, test3.t);
+  StringEDFullUKK edsukk(test3.s, test3.t);
 
-  ASSERT_EQ(test3.costs, edt.costs());
+  int cost_t = edt.compute_editdistance();
+  int cost_r = edr.compute_editdistance();
+  int cost_u = edu.compute_editdistance();
+  int cost_ukk = edukk.compute_editdistance();
+  int cost_dukk = eddukk.compute_editdistance();
+  int cost_sukk = edsukk.compute_editdistance();
+
+  ASSERT_EQ(cost_t, cost_r);
+  ASSERT_EQ(cost_t, cost_u);
+  ASSERT_EQ(cost_t, cost_ukk);
+  ASSERT_EQ(cost_t, cost_dukk);
+  ASSERT_EQ(cost_t, cost_sukk);
 }
 
-////////////////////////////////////////////////////////////////////////////////
 
-TEST(CliverTest, ExecutionTree) {
-  ExecutionTrace::BasicBlockList bb_list;
-  ExecutionTrace etrace1, etrace2;
-
-  etrace2.push_back_bb(
-      new klee::KBasicBlock(
-        llvm::BasicBlock::Create(llvm::getGlobalContext()), 5));
-
-  for(int i=0;i<10;i++) {
-    bb_list.push_back(
-      new klee::KBasicBlock(
-        llvm::BasicBlock::Create(llvm::getGlobalContext()), i));
-    etrace1.push_back_bb(bb_list.back());
-  }
-
-  etrace2.push_back(etrace1);
-  ASSERT_EQ(11, etrace2.size());
-
-}
+//TEST(CliverTest, EditDistance2) {
+//  StringEDT edt(test2.s, test2.t);
+//  StringEDR edr(test2.s, test2.t);
+//
+//  ASSERT_EQ(edt.compute_editdistance(), edr.compute_editdistance());
+//  ASSERT_EQ(test2.costs, edt.costs());
+//}
+//
+//TEST(CliverTest, EditDistance3) {
+//  StringEDT edt(test3.s, test3.t);
+//  StringEDR edr(test3.s, test3.t);
+//
+//  ASSERT_EQ(edt.compute_editdistance(), edr.compute_editdistance());
+//  ASSERT_EQ(test3.costs, edt.costs());
+//}
+//
+//////////////////////////////////////////////////////////////////////////////////
+//
+//TEST(CliverTest, ExecutionTree) {
+//  ExecutionTrace::BasicBlockList bb_list;
+//  ExecutionTrace etrace1, etrace2;
+//
+//  etrace2.push_back_bb(
+//      new klee::KBasicBlock(
+//        llvm::BasicBlock::Create(llvm::getGlobalContext()), 5));
+//
+//  for(int i=0;i<10;i++) {
+//    bb_list.push_back(
+//      new klee::KBasicBlock(
+//        llvm::BasicBlock::Create(llvm::getGlobalContext()), i));
+//    etrace1.push_back_bb(bb_list.back());
+//  }
+//
+//  etrace2.push_back(etrace1);
+//  ASSERT_EQ(11, etrace2.size());
+//
+//}
 
 }
