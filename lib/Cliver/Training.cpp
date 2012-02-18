8eb897fd61c26138ec7a9940ebda45614a63a19b
Robby Cochran <Robby Cochran>
2012-02-18 12:24:15 -0500

    Placed classes ExecutionTrace and TrainingObject into their own files

---
+++ lib/Cliver/Training.cpp
@@ -0,0 +1,87 @@
+//===-- Training.cpp --------------------------------------------*- C++ -*-===//
+//
+// <insert license>
+//
+//===----------------------------------------------------------------------===//
+
+#include "Training.h"
+#include "cliver/ClientVerifier.h"
+#include "cliver/CVExecutionState.h"
+#include "cliver/ExecutionTrace.h"
+#include "cliver/Socket.h"
+#include "CVCommon.h"
+
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+
+namespace cliver {
+
+////////////////////////////////////////////////////////////////////////////////
+
+/// Write Training object to file in cliver's output directory
+void TrainingObject::write(CVExecutionState* state, 
+                           ClientVerifier* cv) {
+
+  // Create an unique identifing name for this path
+  std::stringstream name_ss;
+  name_ss << "round_" << std::setw(4) << std::setfill('0') << cv->round();
+  name_ss << "_length_" << std::setw(6) << std::setfill('0') << trace.size();
+  name_ss << "_state_" <<  state->id() << ".tpath";
+
+  // Set member var
+  name = std::string(name_ss.str());
+
+  // Write object to a sub dir so that # files is not greater than the FS limit
+  std::stringstream subdir_ss;
+  subdir_ss << "round_" << std::setw(4) << std::setfill('0') << cv->round();
+  std::string subdir = subdir_ss.str();
+
+  // Open file ../output_directory/subdir/name
+  std::ostream *file = cv->cvstream()->openOutputFile(name, &subdir);
+
+  // Write to file using boost::serialization
+	boost::archive::binary_oarchive oa(*file);
+  oa << *this;
+
+  // Close file
+  static_cast<std::ofstream*>(file)->close();
+}
+
+/// Read file using boost::serialization
+void TrainingObject::read(std::ifstream &is) {
+	boost::archive::binary_iarchive ia(is);
+  ia >> *this;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool TrainingObjectTraceLT::operator()(const TrainingObject* a, 
+                                       const TrainingObject* b) const {
+  return (a->trace) < (b->trace);
+}
+
+
+bool TrainingObjectLengthLT::operator()(const TrainingObject* a, 
+                                        const TrainingObject* b) const {
+  return a->trace.size() < b->trace.size();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+/// Print TrainingObject info
+std::ostream& operator<<(std::ostream& os, const TrainingObject &tobject) {
+  os << "(trace id:" << tobject.id << ") "
+     << "(length:" << tobject.trace.size() << ") "
+     << "(" << tobject.name << ") ";
+  os << "[socket_events: ";
+  foreach (const SocketEvent* socket_event, tobject.socket_event_set) {
+    os << *socket_event << ", ";
+  }
+  os << "]";
+  return os;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // end namespace cliver
+
---
+++ lib/Cliver/Training.h
@@ -0,0 +1,78 @@
+//===-- Training.h ----------------------------------------------*- C++ -*-===//
+//
+// <insert license>
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//===----------------------------------------------------------------------===//
+#ifndef CLIVER_TRAINING_H
+#define CLIVER_TRAINING_H
+
+#include "cliver/ExecutionTrace.h"
+
+#include <iostream>
+#include <set>
+#include <vector>
+
+namespace boost {void throw_exception(std::exception const& e);}
+#include <boost/serialization/access.hpp>
+#include <boost/serialization/set.hpp>
+#include <boost/serialization/vector.hpp>
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace cliver {
+
+class CVExecutionState;
+class SocketEvent;
+class ClientVerifier;
+
+/// Holds a single execution trace and the associated message
+class TrainingObject {
+ public:
+  TrainingObject() {};
+  TrainingObject(ExecutionTrace *et, SocketEvent *se)
+      : trace(*et) { add_socket_event(se); }
+
+  void add_socket_event(SocketEvent *se) {
+    socket_event_set.insert(se);
+  }
+
+  void read(std::ifstream &is);
+  void write(CVExecutionState* state, ClientVerifier* cv);
+
+ public:
+  std::set<SocketEvent*> socket_event_set; // std::set of SocketEvent ptrs
+  ExecutionTrace trace; 
+  std::string name; // Name created during seralization
+  ExecutionTrace::ID id;
+
+ protected:
+  friend class boost::serialization::access;
+  template<class Archive>
+  void serialize(Archive & ar, const unsigned int version) {
+    ar & socket_event_set;
+    ar & trace;
+    ar & name;
+  }
+};
+
+// Comparator for ExecutionTrace contents
+struct TrainingObjectTraceLT {
+	bool operator()(const TrainingObject* a, const TrainingObject* b) const;
+};
+
+// Comparator for ExecutionTrace lengths
+struct TrainingObjectLengthLT {
+	bool operator()(const TrainingObject* a, const TrainingObject* b) const;
+};
+
+std::ostream& operator<<(std::ostream& os, const TrainingObject &tobject);
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // end namespace cliver
+
+#endif // CLIVER_TRAINING_H
+
