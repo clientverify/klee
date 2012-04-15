861aac6d2787479cb661a7b1ccba079601e60fda
Robby Cochran <rac@cs.unc.edu>
2012-04-15 18:02:52 -0400

    RadixTree and RadixTree unittests

--- include/cliver/RadixTree.h
+++ include/cliver/RadixTree.h
@@ -9,204 +9,339 @@
 #ifndef CLIVER_RADIXTREE_H
 #define CLIVER_RADIXTREE_H
 
+#include <algorithm>
 #include <map>
 
 ////////////////////////////////////////////////////////////////////////////////
-// ArrayType must support substr/erase and array access pattern using brackets,
-// and size()
 
 namespace cliver {
 
-// XXX convert string interface to OrderedCollection interface
-// XXX Substring insertion is a no-op
-// XXX Value storage in RadixEdge, pointer ref or *copy*?
-template <class ArrayType, class ElementType>
+////////////////////////////////////////////////////////////////////////////////
+
+// Helper function for finding size of matching prefix
+template <class InputIterator1, class InputIterator2>
+int prefix_match (InputIterator1 first1, InputIterator1 last1,
+           InputIterator2 first2, InputIterator2 last2) {
+  InputIterator1 it1 = first1;
+  InputIterator2 it2 = first2;
+
+  int count = 0;
+  while (it1 != last1 && it2 != last2) {
+    if (*it1 != *it2)
+      return count;
+    ++it1;
+    ++it2;
+    ++count;
+  }
+  return count;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+            
+template <class SequenceContainer, class ElementType>
 class RadixNode {
-  class REdge {
+  typedef typename SequenceContainer::iterator SequenceContainerIterator;
+
+  class RadixEdge {
    public:
-    REdge(RadixNode* from, RadixNode* to, ArrayType& v) 
-     : from_(from), to_(to), value_(v) /*copy*/ {}
+
+    RadixEdge(RadixNode* from, RadixNode* to, SequenceContainer& s) 
+     : from_(from), to_(to), seq_(s) {}
+
+    RadixEdge(RadixNode* from, RadixNode* to, 
+              SequenceContainerIterator& begin, 
+              SequenceContainerIterator& end) 
+     : from_(from), to_(to), seq_(begin, end) {}
+
     RadixNode* to() { return to_; }
+
     RadixNode* from() { return from_; }
-    ArrayType& value() { return value_; }
-    void set_to(RadixNode* to) { to_ = to; }
+
     void set_from(RadixNode* from) { from_ = from; }
-    void extend(ArrayType &v) {
-      value_ += v;
+    void set_to(RadixNode* to) { to_ = to; }
+
+    void extend(SequenceContainerIterator _begin, 
+                SequenceContainerIterator _end) {
+      seq_.insert(seq_.end(), _begin, _end);
     }
+
+    void erase(SequenceContainerIterator _begin, 
+               SequenceContainerIterator _end) {
+      seq_.erase(_begin, _end);
+    }
+
+    inline ElementType operator[](unsigned i) { return seq_[i]; }
+    inline ElementType operator[](unsigned i) const { return seq_[i]; }
+
+    SequenceContainerIterator begin() { return seq_.begin(); }
+    SequenceContainerIterator end() { return seq_.end(); }
+
+    ElementType key() const { return seq_[0]; }
+
+    size_t size() { return seq_.size(); }
 
    private:
     RadixNode *to_;
     RadixNode *from_;
-    ArrayType value_;
+    SequenceContainer seq_;
   };
 
-  typedef std::map<ElementType, REdge*> TransitionMap;
+  typedef std::map<ElementType, RadixEdge*> TransitionMap;
+
+ private:
+  RadixEdge* get_edge(SequenceContainerIterator it) {
+    if (tmap_.find(*it) != tmap_.end()) {
+      return tmap_[*it];
+    }
+    return NULL;
+  }
+
+  RadixNode* add_edge(SequenceContainerIterator begin,
+                      SequenceContainerIterator end) {
+    RadixNode *node = new RadixNode();
+    RadixEdge *edge = new RadixEdge(this, node, begin, end);
+    node->set_parent_edge(edge);
+    tmap_[*begin] = edge;
+    return node;
+  }
+
+  // Non-recursive, no copying
+  RadixNode* insert(SequenceContainerIterator begin, 
+                    SequenceContainerIterator end) {
+    RadixNode* curr_node = this;
+
+    while (curr_node != NULL) {
+      // Don't do anything if we insert an empty sequence
+      if (begin == end) return curr_node;
+
+      RadixEdge *edge = get_edge(begin);
+
+      // If no transition on s, add new edge to leaf node
+      if (edge == NULL) {
+        return curr_node->add_edge(begin, end);
+      }
+
+      // Find position where match ends between edge and s 
+      int pos = prefix_match(edge->begin(), edge->end(), begin, end);
+
+      // If s is fully matched on this edge, return node it points to
+      if ((begin + pos) == end && pos <= edge->size())
+        return edge->to();
+
+      // If (begin, end) match this edge completely
+      if (pos == edge->size()) {
+
+        // If leaf, just extend edge and return node edge points to
+        if (edge->to()->leaf()) {
+          edge->extend(begin + pos, end);
+          return edge->to();
+        }
+
+        // Otherwise, add rest of s to node that edge points to
+        begin += pos;
+        curr_node = edge->to();
+
+      } else {
+        // Split existing edge
+        RadixNode *split_node = add_edge(edge->begin(), edge->begin() + pos);
+
+        // Erase top of old edge that was just copied
+        edge->erase(edge->begin(), edge->begin() + pos);
+
+        // Update parent to new node
+        edge->set_from(split_node);
+
+        // Update transition map for newly created node
+        split_node->tmap_[edge->key()] = edge;
+
+        // Current node is now split_node
+        begin += pos;
+        curr_node = split_node;
+      }
+    }
+    return curr_node;
+  }
+
 
  public:
 
   RadixNode() : parent_edge_(NULL) {}
 
-  void set_parent_edge(REdge* e) {
-    parent_edge_ = e;
-  }
-
-  REdge* parent_edge() {
-    return parent_edge_;
-  }
-
-  bool root() { return NULL == parent_edge_; }
-  bool leaf() { return 0 == tmap_.size(); }
-
-  RadixNode* lookup(ArrayType &v) {
-    RadixNode* node = this;
-    REdge *edge = NULL;
-    ArrayType tmp_v = v;
-    int match_position = 0;
-
-    while (node != NULL && tmp_v.size() > 0) {
-      if (node->tmap_.find(tmp_v[0]) != node->tmap_.end()) {
-        edge = node->tmap_[tmp_v[0]];
-        match_position = find_match_position(edge->value(), tmp_v);
-        tmp_v = tmp_v.substr(match_position);
-        node = edge->to();
-      } else {
-        node = NULL;
-      }
-    }
-    return node;
-  }
-
-  void remove(ArrayType &v) {
-    RadixNode *node = lookup(v);
-    // if v is not in tree or is present at an internal node we do nothing
-    if (node && node->leaf() && !node->root()) {
-      std::cout << "Removing " << v << "\n";
-
-      REdge *edge = node->parent_edge();
-      RadixNode *parent = edge->from();
-      parent->tmap_.erase(edge->value()[0]);
-      delete edge;
-
-      if (parent->tmap_.size() == 1) {
-        REdge *leaf_edge = parent->tmap_.begin()->second;
-
-        // If leaf_edge->from is not root node
-        if (!leaf_edge->from()->root()) {
-          REdge *parent_edge = leaf_edge->from()->parent_edge();
-          parent_edge->extend(leaf_edge->value());
-
-          // Delete leaf node
-          delete leaf_edge->to();
-          // Delete leaf edge 
-          delete leaf_edge;
-        }
-      }
-    }
-  }
-
-  void insert(ArrayType &v) {
-    // Find if current node has transition on first element of v
-    if (tmap_.find(v[0]) == tmap_.end()) {
-      // Create new node
-      RadixNode *node = new RadixNode();
-      REdge *edge = new REdge(this, node, v);
-      node->set_parent_edge(edge);
-      tmap_[v[0]] = edge;
-      return;
-    }
-
-    // Find pattern match between edge and v 
-    REdge *edge = tmap_[v[0]];
-    int match_position = find_match_position(edge->value(), v);
-    if (match_position == v.size() &&
-        match_position <= edge->value().size()) {
-      return;
-    }
-
-    // Now, either split existing edge or insert at node edge points to
-    if (match_position == edge->value().size()) {
-      ArrayType split_array = v.substr(match_position);
-      if (!edge->to()->leaf()) {
-        edge->to()->insert(split_array);
-      } else {
-        // if not leaf, just extend edge
-        edge->extend(split_array);
-      }
-    } else {
-      RadixNode* split_node = new RadixNode();
-      ArrayType split_array_top = edge->value().substr(0, match_position);
-      REdge *split_edge = new REdge(this, split_node, split_array_top);
-                                    
-      split_node->set_parent_edge(split_edge);
-      tmap_[v[0]] = split_edge;
-
-      // update original edge
-      edge->value().erase(0, match_position);
-      edge->set_from(split_node);
-      split_node->tmap_[edge->value()[0]] = edge;
-
-      if (match_position != v.size()) {
-        ArrayType split_array_bottom = v.substr(match_position);
-        split_node->insert(split_array_bottom);
-      }
-    }
-  }
-
-  int find_match_position(ArrayType &v1, ArrayType &v2) {
-    int i = 0;
-    while (i < v1.size() && i < v2.size()) {
-      if (v1[i] == v2[i])
-        ++i;
-      else
-        break;
-    }
-    return i;
-  }
-
   ~RadixNode() { 
     parent_edge_ = NULL;
   }
 
+  // Return number of outgoing edges
+  int degree() { return tmap_.size(); }
+
+  // Return true if this node has no incoming edge
+  bool root() { return NULL == parent_edge_; }
+
+  // Return true if this node has no outgoing edges
+  bool leaf() { return 0 == tmap_.size(); }
+
+  // Set the incoming edge
+  void set_parent_edge(RadixEdge* e) { parent_edge_ = e; }
+
+  // Return the incoming edge
+  RadixEdge* parent_edge() { return parent_edge_; }
+
+  // Insert a sequence into the tree rooted at this node
+  RadixNode* insert(SequenceContainer &s) {
+    return insert(s.begin(), s.end());
+  }
+
+  /// Lookup the sequence s, starting from the edge at this node, return
+  /// the node that the parent of edge containing the suffix of s.
+  RadixNode* lookup(SequenceContainer &s, bool exact = false) {
+
+    RadixNode* curr_node = this;
+    int pos = 0;
+
+    while (pos < s.size()) {
+
+      // Find matching edge for current element of s
+      if (RadixEdge *edge = curr_node->get_edge(s.begin() + pos)) {
+        size_t remaining = s.size() - pos;
+
+        // Edge size is equal to remaining # elements in s
+        if (edge->size() == remaining) {
+          if (std::equal(edge->begin(), edge->end(), s.begin() + pos))
+            return edge->to();
+          else
+            return NULL;
+          
+        // Edge size is greater than remaining # elements in s
+        } else if (edge->size() > remaining) {
+          if (!exact && std::equal(s.begin() + pos, s.end(), edge->begin()))
+            return edge->to();
+          else
+            return NULL;
+
+        // Edge size is less than remaining # elements in s
+        } else {
+          if (!std::equal(edge->begin(), edge->end(), s.begin() + pos))
+            return NULL;
+        }
+
+        pos += edge->size();
+        curr_node = edge->to();
+
+      } else {
+        // No match in transition map in current node
+        return NULL;
+      }
+    }
+
+    return NULL;
+  }
+
+  // Removes a sequence that terminates at a leaf node, returns true if a remove
+  // actually takes place
+  bool remove(SequenceContainer &s) {
+    // Lookup the node matching s
+    RadixNode *node = lookup(s, /*exact = */ true);
+
+    // If v is not in tree or is present at an internal node, do nothing
+    if (node && node->leaf() && !node->root()) {
+
+      // TODO check length of edge == s
+      RadixEdge *edge = node->parent_edge();
+      RadixNode *parent = edge->from();
+      parent->tmap_.erase(edge->key());
+      delete edge;
+      delete node; 
+      // If parent now only has one child, merge child edge with parent edge
+      // and delete parent
+      if (!parent->root() && parent->degree() == 1) {
+        RadixEdge *merge_edge = parent->tmap_.begin()->second;
+
+        parent->parent_edge()->extend(merge_edge->begin(), merge_edge->end());
+        parent->parent_edge()->set_to(merge_edge->to());
+        parent->parent_edge()->to()->set_parent_edge(parent->parent_edge());
+
+        delete parent;
+        delete merge_edge;
+      }
+      return true;
+    }
+    return false;
+  }
+  
+  RadixNode* extend_parent_edge(SequenceContainer &s) {
+    assert(leaf());
+    parent_edge_->extend(s.begin(), s.end());
+    return this;
+  }
+
+  void print(std::ostream& os, int depth = 0) {
+
+    int parent_edge_size = 0;
+
+    if (parent_edge_) {
+      for (int i = 0; i < depth; ++i)
+        os << " ";
+      for (int i = 0; i < parent_edge_->size(); ++i)
+        os << (*parent_edge_)[i];
+      os << std::endl;
+
+      parent_edge_size = parent_edge_->size();
+    }
+
+    typename TransitionMap::iterator it=tmap_.begin(), iend = tmap_.end();
+    for (; it != iend; ++it) {
+      RadixNode* node = (*(it->second)).to();
+      node->print(os, depth + parent_edge_size);
+    }
+
+  }
+
  private:
-  REdge* parent_edge_;
+  RadixEdge* parent_edge_;
   TransitionMap tmap_;
 };
 
-template <class ArrayType, class ElementType>
+template <class SequenceContainer, class ElementType>
 class RadixTree {
-  typedef RadixNode<ArrayType, ElementType> RNode;
+  typedef RadixNode<SequenceContainer, ElementType> Node;
  public:
   RadixTree() {
-    root_ = new RNode();
+    root_ = new Node();
   }
 
-  ~RadixTree() {}
-  void insert(ArrayType &v) { 
-    root_->insert(v);
+  ~RadixTree() {
+    delete root_;
   }
 
-  void remove(ArrayType &v) {
-    root_->remove(v);
+  Node* insert(SequenceContainer &v) { 
+    return root_->insert(v);
   }
 
-  bool lookup(ArrayType &v) { 
-    RNode *res = root_->lookup(v);
+  Node* extend(Node* node, SequenceContainer &v) { 
+    if (node)
+      return node->extend_parent_edge(v);
+    return NULL;
+  }
+
+  bool remove(SequenceContainer &v) {
+    return root_->remove(v);
+  }
+
+  bool lookup(SequenceContainer &v) { 
+    Node *res = root_->lookup(v);
     if (res) {
-      //if (res->parent_edge())
-      //  std::cout << "Lookup (" << v << ") = " << res->parent_edge()->value() << " \n";
-      //else 
-      //  std::cout << "Lookup (" << v << ") = \"\" \n";
       return true;
     }
-    //std::cout << "Lookup (" << v << ") = NULL  \n";
     return false;
   }
 
-  std::vector<ArrayType>& prefix_matches(ArrayType &v) {}
+  void print(std::ostream& os) {
+    root_->print(os);
+    os << std::endl;
+  }
 
  private:
-  RNode* root_;
+  Node* root_;
 };
 
 
--- unittests/Cliver/RadixTree.cpp
+++ unittests/Cliver/RadixTree.cpp
@@ -22,70 +22,155 @@ using namespace cliver;
 //////////////////////////////////////////////////////////////////////////////////
 
 typedef RadixTree<std::string, char> StringRadixTree;
+typedef RadixTree<std::vector<char>, char> VectorRadixTree;
+
+//////////////////////////////////////////////////////////////////////////////////
+
+class RadixTreeTest : public ::testing::Test {
+ protected:
+
+  virtual void SetUp() {
+    srt = new StringRadixTree();
+    vrt = new VectorRadixTree();
+
+    s1 = "ATest";
+    s2 = "ATestB";
+    s3 = "CTest";
+    s4 = "CTes";
+    s5 = "CTestExtra";
+    s6 = "CTestSuper";
+    s7 = "CTesB";
+    s8 = "CTestSup";
+
+    v1.insert(v1.end(), s1.begin(), s1.end());
+    v2.insert(v2.end(), s2.begin(), s2.end());
+    v3.insert(v3.end(), s3.begin(), s3.end());
+    v4.insert(v4.end(), s4.begin(), s4.end());
+    v5.insert(v5.end(), s5.begin(), s5.end());
+    v6.insert(v6.end(), s6.begin(), s6.end());
+    v7.insert(v7.end(), s7.begin(), s7.end());
+    v8.insert(v8.end(), s8.begin(), s8.end());
+  }
+
+  virtual void TearDown() {
+    delete srt;
+    delete vrt;
+  }
+  
+  StringRadixTree* srt;
+  VectorRadixTree* vrt;
+
+  std::string s_empty, s1, s2, s3, s4, s5, s6, s7, s8;
+
+  std::vector<char> v_empty, v1, v2, v3, v4, v5, v6, v7, v8;
+
+};
+
 
 //////////////////////////////////////////////////////////////////////////////////
 
 namespace {
 
-TEST(RadixTreeTest, CreateAndDestroy) {
-  StringRadixTree *rt = new StringRadixTree();
-  delete rt;
+TEST_F(RadixTreeTest, Init) {
+  ASSERT_TRUE(srt != NULL);
+  ASSERT_TRUE(vrt != NULL);
 }
 
-TEST(RadixTreeTest, Insert) {
-  StringRadixTree *rt = new StringRadixTree();
-  std::string s1("ATestString");
-  std::string s2("ATestBString");
-  std::string s3("");
-  std::string s4("CTestString");
-  std::string s5("CTestStringExtra");
-  std::string s6("CTestStringSuper");
-  rt->insert(s1);
-  rt->insert(s2);
-  rt->insert(s3);
-  rt->insert(s4);
-  rt->insert(s5);
-  rt->insert(s6);
+TEST_F(RadixTreeTest, Insert) {
+  srt->insert(s_empty);
+  srt->insert(s1);
+  srt->insert(s2);
+  srt->insert(s3);
+  srt->insert(s4);
+  srt->insert(s5);
+  srt->insert(s6);
+  srt->insert(s7);
+
+  //srt->print(std::cout);
+
+  vrt->insert(v_empty);
+  vrt->insert(v1);
+  vrt->insert(v2);
+  vrt->insert(v3);
+  vrt->insert(v4);
+  vrt->insert(v5);
+  vrt->insert(v6);
 }
 
-TEST(RadixTreeTest, Lookup) {
-  StringRadixTree *rt = new StringRadixTree();
-  std::string s1("ATestString");
-  std::string s2("ATestBString");
-  std::string s3("");
-  std::string s4("ATestBStr");
-  std::string s5("ATestBStrings");
-  EXPECT_EQ(rt->lookup(s1), false);
-  rt->insert(s1);
-  EXPECT_EQ(rt->lookup(s2), false);
-  rt->insert(s2);
-  EXPECT_EQ(rt->lookup(s1), true);
-  EXPECT_EQ(rt->lookup(s2), true);
-  rt->insert(s3);
-  EXPECT_EQ(rt->lookup(s3), true);
+TEST_F(RadixTreeTest, Lookup) {
+  EXPECT_EQ(srt->lookup(s1), false);
+  srt->insert(s1);
+  EXPECT_EQ(srt->lookup(s2), false);
+  srt->insert(s2);
+  EXPECT_EQ(srt->lookup(s1), true);
+  EXPECT_EQ(srt->lookup(s2), true);
+  srt->insert(s3);
+  EXPECT_EQ(srt->lookup(s3), true);
+  EXPECT_EQ(srt->lookup(s4), true);
+  EXPECT_EQ(srt->lookup(s5), false);
 
-  EXPECT_EQ(rt->lookup(s4), true);
-  EXPECT_EQ(rt->lookup(s5), false);
+  EXPECT_EQ(srt->lookup(s7), false);
+
+  EXPECT_EQ(vrt->lookup(v1), false);
+  vrt->insert(v1);
+  EXPECT_EQ(vrt->lookup(v2), false);
+  vrt->insert(v2);
+  EXPECT_EQ(vrt->lookup(v1), true);
+  EXPECT_EQ(vrt->lookup(v2), true);
+  vrt->insert(v3);
+  EXPECT_EQ(vrt->lookup(v3), true);
+  EXPECT_EQ(vrt->lookup(v4), true);
+  EXPECT_EQ(vrt->lookup(v5), false);
 }
 
-TEST(RadixTreeTest, Remove) {
-  StringRadixTree *rt = new StringRadixTree();
-  std::string s1("ATestString");
-  std::string s2("ATestBString");
-  std::string s3("");
-  std::string s4("ATestBStr");
-  std::string s5("ATestBStrings");
-  EXPECT_EQ(rt->lookup(s1), false);
-  rt->insert(s1);
-  EXPECT_EQ(rt->lookup(s2), false);
-  rt->insert(s2);
-  EXPECT_EQ(rt->lookup(s1), true);
-  EXPECT_EQ(rt->lookup(s2), true);
-  rt->insert(s3);
-  EXPECT_EQ(rt->lookup(s3), true);
+TEST_F(RadixTreeTest, Remove) {
 
-  EXPECT_EQ(rt->lookup(s4), true);
-  EXPECT_EQ(rt->lookup(s5), false);
+  srt->insert(s_empty);
+  srt->insert(s1);
+  srt->insert(s2);
+  srt->insert(s3);
+  srt->insert(s4);
+  srt->insert(s5);
+  srt->insert(s6);
+
+  //srt->print(std::cout);
+  EXPECT_EQ(srt->lookup(s1), true);
+  srt->remove(s2);
+  //srt->print(std::cout);
+  EXPECT_EQ(srt->lookup(s2), false);
+
+  EXPECT_EQ(srt->remove(s8), false);
+  EXPECT_EQ(srt->lookup(s8), true);
+
+  EXPECT_EQ(srt->lookup(s6), true);
+  srt->remove(s6);
+  //srt->print(std::cout);
+  EXPECT_EQ(srt->lookup(s6), false);
+
+  vrt->insert(v_empty);
+  vrt->insert(v1);
+  vrt->insert(v2);
+  vrt->insert(v3);
+  vrt->insert(v4);
+  vrt->insert(v5);
+  vrt->insert(v6);
+  //vrt->print(std::cout);
+
+  //vrt->print(std::cout);
+  EXPECT_EQ(vrt->lookup(v1), true);
+  vrt->remove(v2);
+  //vrt->print(std::cout);
+  EXPECT_EQ(vrt->lookup(v2), false);
+
+  EXPECT_EQ(vrt->remove(v8), false);
+  EXPECT_EQ(vrt->lookup(v8), true);
+
+  EXPECT_EQ(vrt->lookup(v6), true);
+  vrt->remove(v6);
+  //vrt->print(std::cout);
+  EXPECT_EQ(vrt->lookup(v6), false);
+
+
 }
 
 //////////////////////////////////////////////////////////////////////////////////
