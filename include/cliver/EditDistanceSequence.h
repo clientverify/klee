c30e2a7f0604548c8f80389723c5f27bd6289bb6
Robby Cochran <Robby Cochran>
2012-04-18 12:47:15 -0400

    LevenshteinRadixTree now functional with unittests

--- include/cliver/EditDistanceSequence.h
+++ include/cliver/EditDistanceSequence.h
@@ -17,11 +17,17 @@ template< class T >
 class LevenshteinElement {
  public:
   T e;
-  int current;
-  int previous;
+  int d[2];
 
-  LevenshteinElement(T _e) : e(_e), current(0), previous(0) {}
-  LevenshteinElement() : current(0), previous(0) {}
+  LevenshteinElement(T _e, int _d = 0) { 
+    e = _e;
+    d[0] = d[1] = _d;
+  }
+
+  LevenshteinElement() {
+    d[0] = d[1] = 0;
+  }
+
   bool operator==(const LevenshteinElement& le) const { return (e == le.e); }
   bool operator!=(const LevenshteinElement& le) const { return (e != le.e); }
   bool operator< (const LevenshteinElement& le) const { return (e < le.e); }
@@ -29,7 +35,7 @@ class LevenshteinElement {
 
 template< class T > 
 std::ostream& operator<<(std::ostream& os, const LevenshteinElement<T>& le) {
-  os << "(" << le.e << "," << le.previous << ", " << le.current << ")";
+  os << "(" << le.e << "," << le.d[0] << ", " << le.d[1] << ")";
 }
 
 template <class T> 
@@ -88,22 +94,26 @@ class LevenshteinRadixTree
   typedef typename LRadixTree::EdgeMapIterator EdgeMapIterator;
  
 #define foreach_edge(__node, __iterator) \
-  typename EdgeMapIterator __iterator = __node->begin(); \
-  typename EdgeMapIterator __iterator##_END = __node->end(); \
+  EdgeMapIterator __iterator = __node->begin(); \
+  EdgeMapIterator __iterator##_END = __node->end(); \
   for (; __iterator != __iterator##_END; ++__iterator)
 
-  LevenshteinRadixTree() { this->root_ = new Node(); }
-  LevenshteinRadixTree(Node *root) { this->root_ = root; }
+  LevenshteinRadixTree() : curr_(0) { this->root_ = new Node(); }
+  LevenshteinRadixTree(Node *root) : curr_(0) { this->root_ = root; }
 
   // Return a deep-copy of this RadixTree
   virtual LRadixTree* clone() {
-    return new LevenshteinRadixTree(this->clone_node(this->root_));
+    LevenshteinRadixTree *lrt = new LevenshteinRadixTree(this->clone_node(this->root_));
+    lrt->curr_ = this->curr_;
+    return lrt;
   }
 
   virtual bool lookup(Sequence &s) { 
-    LSequence ls(s.size());
-    for (int i=0; i<s.size(); ++i) ls[i].e = s[i];
+    // Convert Sequence into an LevenshteinSequence
+    LSequence ls(s.size() + 1);
+    for (int i=0; i<s.size() + 1; ++i) ls[i].e = (i == 0) ? 0 : s[i - 1];
 
+    // Lookup the Levenshtein sequence
     if (Node *res = this->lookup_private(ls))
       return true;
     return false;
@@ -111,35 +121,39 @@ class LevenshteinRadixTree
 
   // Insert new sequence into this radix tree
   virtual Node* insert(Sequence &s) { 
-    LSequence ls(s.size());
-    for (int i=0; i<s.size(); ++i) {
-      ls[i].e = s[i];
-      ls[i].current = i;
-    } 
+    // Convert Sequence into an LevenshteinSequence
+    LSequence ls(s.size() + 1);
+    for (int i=0; i<s.size() + 1; ++i)
+      ls[i] = (i == 0) ? LElement(0, i): LElement(s[i - 1], i);
+
+    // Insert newly created LevenshteinSequence
     return this->root_->insert(ls);
   }
 
   virtual bool remove(Sequence &s) {
-    LSequence ls(s.size());
-    for (int i=0; i<s.size(); ++i) ls[i].e = s[i];
+    // Convert Sequence into an LevenshteinSequence
+    LSequence ls(s.size()+1);
+    for (int i=0; i<s.size(); ++i) ls[i].e = (i == 0) ? 0 : s[i - 1];
+
     // Lookup the node matching s
     Node *node = this->lookup_private(ls, /*exact = */ true);
+
+    // Remove this node
     return this->remove_node(node);
   }
 
-  void init_distance_values() {
+  void reset_distance_values() {
     // DP Initialization
-    int depth = 0;
     std::stack<Node*> nodes;
     nodes.push(this->root_);
     while (!nodes.empty()) {
-      Node* curr_node = nodes.top();
+      Node* n = nodes.top();
       nodes.pop();
-      foreach_edge(curr_node, it) {
+      foreach_edge(n, it) {
         Edge *edge = it->second;
-        int depth = curr_node->depth();
+        int depth = n->depth();
         for (int i=0; i<edge->size(); ++i) {
-          (*edge)[i].current = depth++;
+          (*edge)[i].d[0] = depth++;
         }
         if (!edge->to()->leaf())
           nodes.push(edge->to());
@@ -147,41 +161,85 @@ class LevenshteinRadixTree
     }
   }
 
+  virtual int lookup_cost(Sequence &s) { 
+    // Convert Sequence into an LevenshteinSequence
+    LSequence ls(s.size() + 1);
+    for (int i=0; i<s.size() + 1; ++i) ls[i].e = (i == 0) ? 0 : s[i - 1];
+
+    // Lookup the Levenshtein sequence
+    if (Node *res = this->lookup_private(ls, /*exact = */ true)) {
+      LElement& e = res->parent_edge()->last_element();
+      return e.d[curr_];
+    } else if (Node *res = this->lookup_private(ls)) {
+      size_t diff = res->depth() - ls.size();
+      Edge *edge = res->parent_edge();
+      LElement& e = (*edge)[edge->size() - diff - 1];
+      return e.d[curr_];
+    }
+    return -1 ;
+  }
+
+  // Fill out edit distance values for a given sequence and return the minimum
+  // cost edit distance found in the tree
   int min_edit_distance(Sequence &s) {
     int min_cost = INT_MAX;
 
-    init_distance_values();
+    LElement root_element(0);
 
-    for (int j=0; j < s.size(); ++j) {
+    for (int j=0; j < s.size()+1; ++j) {
       // walk tree from root and compute edit distance
       std::stack<Node*> nodes;
       nodes.push(this->root_);
 
+      curr_ = j % 2;
+
       while (!nodes.empty()) {
-        Node* curr_node = nodes.top();
+        Node* n = nodes.top();
         nodes.pop();
-        foreach_edge(curr_node, it) {
+
+        foreach_edge(n, it) {
           Edge *edge = it->second;
 
-          LElement *e0, *e1;
-          for (int i=0; i < edge->size(); ++i) {
-            if (i == 0) {
-              Edge* parent_edge = curr_node->parent_edge();
-              e0 = &((*parent_edge)[parent_edge->size()-1]);
-            } else {
-              e0 = &((*edge)[i-1]);
-            }
-            e1 = &((*edge)[i]);
-            int ins_or_del = std::min(e0->current + 1, e1->previous + 1);
-            int replace = (s[j] == e1->e) ? e0->previous : e0->previous + 1;
-            e1->previous = e1->current;
-            e1->current = std::min(ins_or_del, replace);
-          }
+          if (j == 0) {
 
-          if (edge->to()->leaf() && (e1->current < min_cost) ) {
-            min_cost = e1->current;
+            int depth = n->depth();
+            for (int i=0; i<edge->size(); ++i)
+              (*edge)[i].d[curr_] = depth++;
+
+            if (!edge->to()->leaf())
+              nodes.push(edge->to());
+
           } else {
-            nodes.push(edge->to());
+
+            T t = s[j-1];
+            LElement *e0, *e1;
+
+            for (int i=0; i<edge->size(); ++i) {
+
+              e1 = &((*edge)[i]);
+
+              if (i == 0 && n == this->root_) {
+                e1->d[curr_] = j;
+              } else {
+
+                if (i == 0)
+                  e0 = &(n->parent_edge()->last_element());
+                else
+                  e0 = &((*edge)[i-1]);
+
+                int ins_or_del 
+                  = std::min(e0->d[curr_] + 1, e1->d[curr_^1] + 1);
+                int match_or_replace 
+                  = (t == e1->e) ? e0->d[curr_^1] : e0->d[curr_^1] + 1;
+
+                e1->d[curr_] = std::min(ins_or_del, match_or_replace);
+              }
+            }
+
+            if (j == s.size() && edge->to()->leaf() && (e1->d[curr_] < min_cost) )
+              min_cost = e1->d[curr_];
+            else
+              nodes.push(edge->to());
           }
         }
       }
@@ -190,7 +248,7 @@ class LevenshteinRadixTree
   }
 
  private:
-  bool initialized_;
+  int curr_; // either 0 or 1
 };
 
 
