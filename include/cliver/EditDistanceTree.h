fcca913b4a234a0c4b50d97398fad0c2a0f10422
Robby Cochran <Robby Cochran>
2012-05-05 00:23:16 -0400

    Added EditDistanceTree interface

---
+++ include/cliver/EditDistanceTree.h
@@ -0,0 +1,36 @@
+//===-- EditDistanceTree.h ----------------------------------*- C++ -*-===//
+//
+// <insert license>
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//===----------------------------------------------------------------------===//
+#ifndef CLIVER_EDIT_DISTANCE_TREE_H
+#define CLIVER_EDIT_DISTANCE_TREE_H
+
+////////////////////////////////////////////////////////////////////////////////
+// Abstract edit distance tree interface
+////////////////////////////////////////////////////////////////////////////////
+
+namespace cliver {
+
+template<class Sequence, class Element>
+class EditDistanceTree {
+ public:
+  virtual void init(int k) = 0;
+  virtual void add_data(Sequence &s) = 0;
+  virtual void update(Sequence &s_update) = 0;
+  virtual void update_suffix(Sequence &s) = 0;
+  virtual void update_element(Element e) = 0;
+  virtual int  min_distance() = 0;
+  virtual void prepare_delete() = 0;
+  virtual EditDistanceTree* clone_edit_distance_tree() = 0;
+};
+
+} // end namespace cliver
+
+////////////////////////////////////////////////////////////////////////////////
+
+#endif // CLIVER_EDIT_DISTANCE_TREE_H
+
