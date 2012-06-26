b2730823a8481d7ee82b80ecda229de760d65a0e
Robby Cochran <rac@cs.unc.edu>
2012-06-26 13:00:08 -0400

    Searcher: use correct PriorityQueue search when training

--- lib/Cliver/ExecutionTraceManager.cpp
+++ lib/Cliver/ExecutionTraceManager.cpp
@@ -262,7 +262,7 @@ void TrainingExecutionTraceManager::notify(ExecutionEvent ev) {
           stages_.count(parent_property) && 
           stages_[parent_property]->etrace_tree->tracks(parent_property)) {
 
-        CVDEBUG("End state: " << *parent);
+        CVMESSAGE("NEW STAGE at state: " << *parent);
 
         new_stage->parent_stage = stages_[parent_property];
 
