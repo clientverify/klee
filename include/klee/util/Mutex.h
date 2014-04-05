fe24a97e544ec7a568f607887d35638b4bdc3f85
Robby Cochran <rac@cs.unc.edu>
2014-04-05 01:08:59 -0400

    Add SpinLock implemention and typedefs for boost lockfree data structures

--- include/klee/util/Mutex.h
+++ include/klee/util/Mutex.h
@@ -10,6 +10,11 @@
 #ifndef KLEE_MUTEX_H
 #define KLEE_MUTEX_H
 
+#include "klee/util/Atomic.h"
+
+#include <boost/lockfree/queue.hpp>
+#include <boost/lockfree/stack.hpp>
+
 #include <boost/thread/barrier.hpp>
 #include <boost/thread/condition_variable.hpp>
 #include <boost/thread/lock_guard.hpp>
@@ -20,15 +25,55 @@ namespace boost { void throw_exception(std::exception const& e); }
 
 namespace klee {
 
+class SpinLock {
+private:
+  Atomic<bool>::type state;
+public:
+  SpinLock() : state(false) {}
+  void lock() {
+#ifdef ENABLE_BOOST_ATOMIC
+    while (state.exchange(true, boost::memory_order_acquire) == true)
+        ;
+#else
+    while (state == true)
+        ;
+#endif
+  }
+  void unlock() {
+#ifdef ENABLE_BOOST_ATOMIC
+    state.store(false, boost::memory_order_release);
+#else
+    state = false;
+#endif
+  }
+};
+
 typedef boost::mutex Mutex;
 typedef boost::recursive_mutex RecursiveMutex;
 
 typedef boost::lock_guard<RecursiveMutex> RecursiveLockGuard;
 typedef boost::lock_guard<Mutex> LockGuard;
+typedef boost::lock_guard<SpinLock> SpinLockGuard;
 typedef boost::unique_lock<Mutex> UniqueLock;
+
+template<class T>
+struct Guard {
+  typedef boost::lock_guard<T> type;
+};
 
 typedef boost::barrier Barrier;
 typedef boost::condition_variable ConditionVariable;
+
+template<class T>
+struct LockFreeStack {
+  typedef boost::lockfree::stack<T> type;
+};
+
+template<class T>
+struct LockFreeQueue {
+  typedef boost::lockfree::queue<T> type;
+};
+
 
 } // end namespace klee
 
