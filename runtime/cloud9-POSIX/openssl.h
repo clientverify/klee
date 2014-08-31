5cf8552501bb1ed1c7b26005802f0f7845d4acb8
Robby Cochran <Robby Cochran>
2014-08-31 12:14:27 -0400

    Add symbolic models for selected OpenSSL functions

---
+++ runtime/cloud9-POSIX/openssl.h
@@ -0,0 +1,52 @@
+#ifndef OPENSSL_H_
+#define OPENSSL_H_
+
+#include "common.h"
+
+// FIXME: need internal openssl data types
+#include "../../../openssl/include/openssl/ssl.h"
+#include "../../../openssl/include/openssl/ssl3.h"
+#include "../../../openssl/include/openssl/sha.h"
+#include "../../../openssl/include/openssl/ec.h"
+#include "../../../openssl/include/openssl/ossl_typ.h"
+#include "../../../openssl/include/openssl/modes.h"
+#include "../../../openssl/crypto/ec/ec_lcl.h"
+#include "../../../openssl/crypto/modes/modes_lcl.h"
+#include "../../../openssl/crypto/sha/sha.h"
+
+// Enable debug output
+#define DEBUG_OPENSSL_MODEL 0
+
+// Enable for fully concrete model (requires ktest)
+#define KTEST_RAND_PLAYBACK 0
+#define KTEST_SELECT_PLAYBACK 0
+#define KTEST_STDIN_PLAYBACK 0
+
+DECLARE_MODEL(int, RAND_bytes, unsigned char *buf, int num)
+DECLARE_MODEL(int, RAND_pseudo_bytes, unsigned char *buf, int num)
+
+DECLARE_MODEL(int, EC_KEY_generate_key, EC_KEY *eckey)
+DECLARE_MODEL(int, ECDH_compute_key, void *out, size_t outlen, const EC_POINT *pub_key, EC_KEY *eckey, void *(*KDF)(const void *in, size_t inlen, void *out, size_t *outlen))
+DECLARE_MODEL(int, tls1_generate_master_secret, SSL *s, unsigned char *out, unsigned char *p, int len)
+DECLARE_MODEL(size_t, EC_POINT_point2oct, const EC_GROUP *group, const EC_POINT *point, point_conversion_form_t form, unsigned char *buf, size_t len, BN_CTX *ctx)
+
+DECLARE_MODEL(int, SHA1_Update, SHA_CTX *c, const void *data, size_t len)
+DECLARE_MODEL(int, SHA1_Final, unsigned char *md, SHA_CTX *c)
+DECLARE_MODEL(int, SHA256_Update, SHA256_CTX *c, const void *data, size_t len)
+DECLARE_MODEL(int, SHA256_Final, unsigned char *md, SHA256_CTX *c)
+
+DECLARE_MODEL(int, ktest_select, int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout)
+
+DECLARE_MODEL(void, CRYPTO_gcm128_init, GCM128_CONTEXT *ctx, void *key, block128_f block)
+DECLARE_MODEL(void, CRYPTO_gcm128_setiv, GCM128_CONTEXT *ctx,const unsigned char *iv,size_t len)
+DECLARE_MODEL(int, CRYPTO_gcm128_aad, GCM128_CONTEXT *ctx,const unsigned char *aad,size_t len)
+DECLARE_MODEL(int, CRYPTO_gcm128_encrypt, GCM128_CONTEXT *ctx, const unsigned char *in, unsigned char *out, size_t len)
+DECLARE_MODEL(void, CRYPTO_gcm128_tag, GCM128_CONTEXT *ctx, unsigned char *tag, size_t len)
+DECLARE_MODEL(int, CRYPTO_gcm128_finish, GCM128_CONTEXT *ctx,const unsigned char *tag, size_t len)
+
+DECLARE_MODEL(int, EVP_CipherInit, EVP_CIPHER_CTX *ctx, const EVP_CIPHER *cipher, const unsigned char *key, const unsigned char *iv, int enc)
+DECLARE_MODEL(int, EVP_Cipher, EVP_CIPHER_CTX *ctx, unsigned char *out, const unsigned char *in, unsigned int inl)
+DECLARE_MODEL(int, EVP_CipherUpdate, EVP_CIPHER_CTX *ctx, unsigned char *out, int *outl, const unsigned char *in, int inl)
+DECLARE_MODEL(int, EVP_CipherFinal, EVP_CIPHER_CTX *ctx, unsigned char *outm, int *outl)
+
+#endif /* OPENSSL_H_ */
---
+++ runtime/cloud9-POSIX/sockets_cliver.c
@@ -0,0 +1,184 @@
+//===-- socket.c -====-----------------------------------------------------===//
+//
+// <insert license>
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//===----------------------------------------------------------------------===//
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <assert.h>
+
+#include <time.h> 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+// override inline assembly version of FD_ZERO from
+// /usr/include/x86_64-linux-gnu/bits/select.h
+#ifdef FD_ZERO
+#undef FD_ZERO
+#endif
+#define FD_ZERO(p)        memset((char *)(p), 0, sizeof(*(p)))
+
+static void __emit_error(const char *msg) {
+  klee_report_error(__FILE__, __LINE__, msg, "user.err");
+}
+
+// NOTE: This network model is INCOMPLETE
+
+// POSIX implementation of htons(3) 
+//uint16_t htons(uint16_t hostshort) { 
+//unsigned short htons(unsigned short hostshort) { 
+//  return hostshort; 
+//}
+
+// POSIX implementation of inet_pton(3) 
+//int inet_pton(int af, const char *src, void *dst) {
+//  return 1;
+//}
+
+// POSIX implementation of connect(2)
+int connect(int sockfd, const struct sockaddr *serv_addr, socklen_t addrlen) {
+  klee_warning("called connect()");
+  return 0;
+}
+
+// POSIX implementation of shutdown(2) 
+int shutdown(int sockfd, int how) {
+  klee_warning("called shutdown()");
+  return cliver_socket_shutdown(sockfd, how);
+}
+
+// POSIX implementation of socket(2) 
+int socket(int domain, int type, int protocol) {
+  klee_warning("called socket()");
+  return cliver_socket_create(domain, type, protocol);
+}
+
+int bind(int sockfd, const struct sockaddr *my_addr, socklen_t addrlen) {
+  klee_warning("called bind()");
+  return 0;
+}
+
+ssize_t send(int s, const void *buf, size_t len, int flags) {
+  klee_warning("called send()");
+  return cliver_socket_write(s, buf, len, flags);
+}
+
+ssize_t sendto(int s, const void *buf, size_t len, int flags, 
+               const struct sockaddr *to, socklen_t tolen) {
+  klee_warning("called sendto()");
+  return cliver_socket_write(s, buf, len, flags);
+}
+
+ssize_t recv(int s, void *buf, size_t len, int flags) {
+  klee_warning("called recv()");
+  return cliver_socket_read(s, buf, len, flags);
+}
+
+#define SOCKET_REPLAY_ID  1000
+#define SOCKET_REPLAY_SERVER_PORT 9999
+#define SOCKET_REPLAY_SERVER_NAME "localhost"
+
+ssize_t recvfrom(int s, void *buf, size_t len, int flags,
+                 struct sockaddr *from, socklen_t *fromlen) {
+  klee_warning("called recvfrom()");
+  struct sockaddr_in *addr = (struct sockaddr_in*)from;
+  addr->sin_family = AF_INET;
+  addr->sin_port = SOCKET_REPLAY_SERVER_PORT;
+  inet_pton(AF_INET, SOCKET_REPLAY_SERVER_NAME, &addr->sin_addr);
+  *fromlen = sizeof(struct sockaddr_in);
+
+  return cliver_socket_read(s, buf, len, flags);
+}
+
+int getsockname(int s, struct sockaddr *name, socklen_t *namelen) {
+  klee_warning("called getsockname()");
+
+  unsigned short port;
+  klee_make_symbolic(&port, sizeof(unsigned short), "getsockname_port");
+  ((struct sockaddr_in*)name)->sin_port = port;
+  return 0;
+}
+
+int setsockopt(int s, int level, int optname, 
+               const void *optval, socklen_t optlen) {
+  klee_warning("called setsockopt()");
+  return 0;
+}
+
+/*
+int gettimeofday(struct timeval *tv, struct timezone *tz) {
+  klee_warning("called gettimeofday()");
+  if (tv == NULL) 
+    return -1; 
+  klee_warning_once("using symbolic time model in gettimeofday");
+  klee_make_symbolic(tv, sizeof(struct timeval), "gettimeofday");
+  return 0;
+}
+*/
+
+time_t __time(time_t *t) {
+  klee_warning("called __time()");
+  time_t __t;
+  klee_make_symbolic(&__t, sizeof(time_t), "__time");
+
+  if (t) 
+		t = &__t;
+
+  return __t;
+}
+
+int cliver_training_start() {
+  return 0;
+}
+
+// Network capture for Cliver
+int __klee_model_ktest_connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+  klee_warning("called __klee_model_ktest_connect()");
+
+  printf("connect() called on socket for TLS traffic (%d)\n", sockfd);
+  return 0;
+}
+
+ssize_t __klee_model_ktest_writesocket(int fd, const void *buf, size_t count) {
+  unsigned i;
+  klee_warning("called __klee_model_ktest_writesocket()");
+
+  // HACK NetworkManager doesn't support non-ObjectState aligned pointers
+  //return cliver_socket_write(fd, buf, count, 0);
+  
+  char *buf_ptr = buf;
+  char *buf_copy = (char*)malloc(count);
+
+  for (i=0; i<count; ++i) {
+    buf_copy[i] = buf_ptr[i];
+  }
+  int res = cliver_socket_write(fd, buf_copy, count, 0);
+  free(buf_copy);
+  return res;
+}
+
+ssize_t __klee_model_ktest_readsocket(int fd, void *buf, size_t count) {
+  unsigned i;
+  klee_warning("called __klee_model_ktest_readsocket()");
+
+  // HACK NetworkManager doesn't support non-ObjectState aligned pointers
+  //return cliver_socket_read(fd, buf, count, 0);
+  
+  char *buf_ptr = buf;
+  char *buf_copy = (char*)malloc(count);
+
+  int res = cliver_socket_read(fd, buf_copy, count, 0);
+  for (i=0; i<res; ++i) {
+    buf_ptr[i] = buf_copy[i];
+  }
+  free(buf_copy);
+  return res;
+}
