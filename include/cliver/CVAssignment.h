d36d9596952557ae6acdf190b5a90625b6b0a104
Robby Cochran <rac@cs.unc.edu>
2014-12-01 14:50:53 -0500

    Added CVAssignment class - Used during network events in cliver to concretize symbolic variables   based on network log data - Used during multi-pass to lookup and retrieve concretizations for   symbolic variables when created on subsequent multipass executions

---
+++ include/cliver/CVAssignment.h
@@ -0,0 +1,75 @@
+//===-- CVAssignment.cpp ----------------------------------------*- C++ -*-===//
+//
+// <insert license>
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CV_ASSIGNMENT_H
+#define CV_ASSIGNMENT_H
+
+#include "klee/util/Assignment.h"
+#include "klee/Solver.h"
+
+#include <ostream>
+
+namespace cliver {
+class CVAssignment : public klee::Assignment {
+ public:
+  typedef std::map<std::string, const klee::Array*> name_ty;
+
+  name_ty name_bindings;
+
+  CVAssignment() : klee::Assignment(false) {}
+
+  CVAssignment(klee::ref<klee::Expr> &e);
+
+  CVAssignment(std::vector<const klee::Array*> &objects, 
+               std::vector< std::vector<unsigned char> > &values);
+
+  void solveForBindings(klee::Solver* solver, 
+                        klee::ref<klee::Expr> &expr);
+
+  void addBindings(std::vector<const klee::Array*> &objects, 
+                   std::vector< std::vector<unsigned char> > &values);
+
+  const klee::Array* getArray(std::string &name) {
+    if (name_bindings.find(name) != name_bindings.end()) {
+      return name_bindings[name];
+    }
+    return NULL;
+  }
+
+  std::vector<unsigned char>* getBindings(std::string &name) {
+    if (name_bindings.find(name) != name_bindings.end()) {
+      return &(bindings[name_bindings[name]]);
+    }
+    return NULL;
+  }
+
+  bool has(std::string &name) {
+    return name_bindings.find(name) != name_bindings.end();
+  }
+
+  void clear() {
+    bindings.clear();
+    name_bindings.clear();
+  }
+
+  void print(std::ostream& os) const;
+
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+std::ostream &operator<<(std::ostream &os, const CVAssignment &x);
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // End cliver namespace
+
+#endif
+
+
---
+++ lib/Cliver/CVAssignment.cpp
@@ -0,0 +1,162 @@
+//===-- CVAssignement.cpp ---------------------------------------*- C++ -*-===//
+//
+// <insert license>
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//===----------------------------------------------------------------------===//
+
+#include "cliver/CVAssignment.h"
+#include "cliver/CVStream.h"
+
+#include "klee/util/ExprUtil.h"
+#include "klee/Constraints.h"
+#include "../Core/ImpliedValue.h"
+#include "../Core/Common.h"
+
+#include "llvm/Support/CommandLine.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+llvm::cl::opt<bool>
+DebugCVAssignment("debug-cv-assignment", llvm::cl::init(false));
+
+#ifndef NDEBUG
+
+#undef CVDEBUG
+#define CVDEBUG(x) \
+	__CVDEBUG(DebugCVAssignment, x);
+
+#undef CVDEBUG_S
+#define CVDEBUG_S(__state_id, __x) \
+	__CVDEBUG_S(DebugCVAssignment, __state_id, __x)
+
+#endif
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace cliver {
+
+CVAssignment::CVAssignment(std::vector<const klee::Array*> &objects, 
+                           std::vector< std::vector<unsigned char> > &values) {
+  addBindings(objects, values);
+}
+
+void CVAssignment::addBindings(std::vector<const klee::Array*> &objects, 
+                                std::vector< std::vector<unsigned char> > &values) {
+
+  std::vector< std::vector<unsigned char> >::iterator valIt = 
+    values.begin();
+  for (std::vector<const klee::Array*>::iterator it = objects.begin(),
+          ie = objects.end(); it != ie; ++it) {
+    const klee::Array *os = *it;
+    std::vector<unsigned char> &arr = *valIt;
+    bindings.insert(std::make_pair(os, arr));
+    name_bindings.insert(std::make_pair(os->name, os));
+    ++valIt;
+  }
+}
+
+void CVAssignment::solveForBindings(klee::Solver* solver, 
+                                    klee::ref<klee::Expr> &expr) {
+  std::vector<const klee::Array*> arrays;
+  std::vector< std::vector<unsigned char> > initial_values;
+
+  klee::findSymbolicObjects(expr, arrays);
+  klee::ConstraintManager cm;
+  cm.addConstraint(expr);
+
+  klee::Query query(cm, klee::ConstantExpr::alloc(0, klee::Expr::Bool));
+  solver->getInitialValues(query, arrays, initial_values);
+
+  // Print implied values
+  for (unsigned i=0; i<arrays.size(); ++i) {
+    std::ostringstream info;
+    info << "IV: ARRAY(" << initial_values[i].size() << ") "
+         << arrays[i]->name << " = ";
+
+    bool isASCII = true;
+    for (unsigned j=0; j<initial_values[i].size()-1; ++j) {
+      if (initial_values[i][j] > 128)
+        isASCII = false;
+    }
+
+    if (isASCII) {
+      info << std::string(initial_values[i].begin(), initial_values[i].end());
+    } else {
+      info << "0x";
+      for (unsigned j=0; j<initial_values[i].size(); ++j) {
+        unsigned c = initial_values[i][j];
+        info << std::hex << c << std::dec;
+      }
+    }
+
+    CVDEBUG(info.str());
+  }
+
+  klee::ref<klee::Expr> value_disjunction
+		= klee::ConstantExpr::alloc(0, klee::Expr::Bool);
+
+  for (unsigned i=0; i<arrays.size(); ++i) {
+    for (unsigned j=0; j<initial_values[i].size(); ++j) {
+
+      klee::ref<klee::Expr> read = 
+        klee::ReadExpr::create(klee::UpdateList(arrays[i], 0),
+          klee::ConstantExpr::create(j, klee::Expr::Int32));
+
+      klee::ref<klee::Expr> neq_expr = 
+        klee::NotExpr::create(
+          klee::EqExpr::create(read,
+            klee::ConstantExpr::create(initial_values[i][j], klee::Expr::Int8)));
+
+      value_disjunction = klee::OrExpr::create(value_disjunction, neq_expr);
+    }
+  }
+
+  value_disjunction = cm.simplifyExpr(value_disjunction);
+
+  if (value_disjunction->getKind() == klee::Expr::Constant
+      && cast<klee::ConstantExpr>(value_disjunction)->isFalse()) {
+    CVDEBUG("Concretization must be true!");
+    addBindings(arrays, initial_values);
+  } else {
+
+    cm.addConstraint(value_disjunction);
+
+    bool result;
+    solver->mayBeTrue(klee::Query(cm,
+      klee::ConstantExpr::alloc(0, klee::Expr::Bool)), result);
+
+    if (result) {
+      CVDEBUG("INVALID solver concretization!");
+    } else {
+      //TODO test this path
+      cv_warning("CVAssignement: TODO verify logic for this path ");
+      CVDEBUG("VALID solver concretization!");
+      addBindings(arrays, initial_values);
+    }
+  }
+}
+
+void CVAssignment::print(std::ostream& os) const {
+  for (name_ty::const_iterator it=name_bindings.begin(),ie=name_bindings.end();
+       it!=ie; ++it) {
+    os << it->first;
+    name_ty::const_iterator next = it;
+    if (++next != ie)
+      os << ", ";
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+std::ostream &operator<<(std::ostream &os, const CVAssignment &x) {
+  x.print(os);
+  return os;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // end namespace cliver
