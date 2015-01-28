6f1006eeb21afb1f2afafc1cc6dc45879ce0a352
Robby Cochran <rac@cs.unc.edu>
2015-01-27 23:04:39 -0500

    [Thread] add generic encapsulation for boost::call_once and support

--- include/klee/util/Thread.h
+++ include/klee/util/Thread.h
@@ -13,14 +13,19 @@
 #include "klee/util/Atomic.h"
 #include <boost/thread/thread.hpp>
 #include <boost/thread/tss.hpp>
+#include <boost/thread/once.hpp>
 
 #define BOOST_LEXICAL_CAST_ASSUME_C_LOCALE
 #include <boost/lexical_cast.hpp>
+
+#define ONCE_FLAG_INIT BOOST_ONCE_INIT 
+#define call_once boost::call_once
 
 namespace klee {
 
 typedef boost::thread Thread;
 typedef boost::thread_group ThreadGroup;
+typedef boost::once_flag OnceFlag;
 
 template<class T>
 struct ThreadSpecificPointer {
