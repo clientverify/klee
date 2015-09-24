32ef8cffc3c089d0536c9f3177ecd59bb2596207
Robby Cochran <rac@cs.unc.edu>
2015-09-23 21:28:26 -0400

    [test] now run all  TestClientServerParallel test in parallel!

--- test/Cliver/TestClientServerParallel.c
+++ test/Cliver/TestClientServerParallel.c
@@ -5,13 +5,13 @@
 // RUN: grep -q "SERVER: success" %t1.log
 // RUN: not grep -q "error" %t1.log
 // RUN: rm -rf %t1.cliver-out
-// RUN: %klee -use-threads=4 -output-dir=%t1.cliver-out -cliver -optimize=0 -posix-runtime -libc=uclibc -socket-log=%t1.ktest %t1.bc &> %t1.cliver.log
+// RUN: %klee -use-threads=4 -use-forked-solver=0 -output-dir=%t1.cliver-out -cliver -optimize=0 -posix-runtime -libc=uclibc -socket-log=%t1.ktest %t1.bc &> %t1.cliver.log
 // RUN: grep -q "CLIENT: success" %t1.cliver.log
 // UN: rm -rf %t2.cliver-out
-// UN: %klee -cliver -cliver-mode=training -optimize=0 -posix-runtime -libc=uclibc -output-dir=%t2.cliver-out -socket-log=%t1.ktest %t1.bc &> %t2.cliver.log
+// UN: %klee -use-threads=4 -use-forked-solver=0 -cliver -cliver-mode=training -optimize=0 -posix-runtime -libc=uclibc -output-dir=%t2.cliver-out -socket-log=%t1.ktest %t1.bc &> %t2.cliver.log
 // UN: grep -q "CLIENT: success" %t2.cliver.log
 // UN: rm -rf %t3.cliver-out
-// UN: %klee -cliver -use-clustering -cliver-mode=edit-dist-kprefix-row -optimize=0 -posix-runtime -libc=uclibc -output-dir=%t3.cliver-out -training-path-dir=%t2.cliver-out -socket-log=%t1.ktest %t1.bc &> %t3.cliver.log
+// UN: %klee -use-threads=4 -use-forked-solver=0 -cliver -use-clustering -cliver-mode=edit-dist-kprefix-row -optimize=0 -posix-runtime -libc=uclibc -output-dir=%t3.cliver-out -training-path-dir=%t2.cliver-out -socket-log=%t1.ktest %t1.bc &> %t3.cliver.log
 // UN: not grep -q "Recomputed kprefix" %t3.cliver.log
 // UN: grep -q "CLIENT: success" %t3.cliver.log
 // REQUIRES: threads
