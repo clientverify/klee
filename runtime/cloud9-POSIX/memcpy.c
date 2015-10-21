03f78227f4c16a911ec342caf72b7f0446d76a5b
Andrew Chi <achi@cs.unc.edu>
2015-10-21 16:36:38 -0400

    Make intrinsic memcpy/memset work with uclibc
    uclibc depends on undefined behavior of memcpy (and probably memset). We
    reluctantly make our model conform to those expectations.

--- runtime/cloud9-POSIX/memcpy.c
+++ runtime/cloud9-POSIX/memcpy.c
@@ -12,6 +12,14 @@
 
 #ifdef HAVE_MEMORY_FUNCTION_INTRINSICS
 DEFINE_MODEL(void*, memcpy, void *dst, void const *src, size_t len) {
+
+  // uclibc depends on the following behavior, although technically passing an
+  // invalid pointer to memcpy causes undefined behavior according to the C
+  // standard.
+  if (len == 0) {
+    return dst; // even if dst is NULL or otherwise invalid
+  }
+
   return klee_memcpy(dst, src, len);
 }
 #endif
--- runtime/cloud9-POSIX/memset.c
+++ runtime/cloud9-POSIX/memset.c
@@ -12,6 +12,14 @@
 
 #ifdef HAVE_MEMORY_FUNCTION_INTRINSICS
 DEFINE_MODEL(void*, memset, void *dst, int s, size_t count) {
+
+  // uclibc depends on the following behavior, although technically passing an
+  // invalid pointer to memset causes undefined behavior according to the C
+  // standard.
+  if (count == 0) {
+    return dst; // even if dst is NULL or otherwise invalid
+  }
+
   return klee_memset(dst, s, count);
 }
 #endif
