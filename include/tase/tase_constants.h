35ee557a36c47949f88c365bb2db4d1e36c68bd8
Kartik Null Cating-Subramanian <kncs@cs.unc.edu>
2018-09-17 03:52:45 -0400

    Move header files into tase/ directory to simplify include paths

--- include/tase/tase_constants.h
+++ include/tase/tase_constants.h
@@ -1,5 +1,11 @@
+#ifndef TASE_CONSTANTS_H
+#define TASE_CONSTANTS_H
+
 #define BUFFER_SIZE 256
 #define STACK_SIZE 2097152
+
 #define STDIN_FD 0
 #define SOCKET_FD -1 // Need to determine a valid val
 //or make it a static global.
+
+#endif
